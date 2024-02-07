c9961178be2062ab15d4782e81872c0d2b13242e
ldynia <change@me.com>
2024-02-07 11:54:25 +0100

    Refactor codebase and update docs

--- libraries/python/standardwebhooks/__init__.py
+++ libraries/python/standardwebhooks/__init__.py
@@ -1,8 +1,17 @@
-from .webhooks import Webhook, WebhookVerificationError
+from .config import WEBHOOK_SECRET_ENCODING
+from .config import WEBHOOK_SECRET_PREFIX
+from .config import WEBHOOK_TOLERANCE_SECONDS
+from .config import WEBHOOK_VERSION
+from .webhooks import Webhook
+from .webhooks import WebhookVerificationError
 
 __all__ = [
+    "WEBHOOK_SECRET_ENCODING",
+    "WEBHOOK_SECRET_PREFIX",
+    "WEBHOOK_TOLERANCE_SECONDS",
+    "WEBHOOK_VERSION",
     "Webhook",
     "WebhookVerificationError",
 ]
 
-__version__ = "1.3.0"
+__version__ = "1.1.0"
