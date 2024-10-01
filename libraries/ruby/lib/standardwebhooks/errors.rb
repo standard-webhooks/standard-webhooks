4a36dc80ba410ed5a6331d7d0928093ed6cf8022
sandstrom <sandstrom@users.noreply.github.com>
2024-10-01 10:42:55 +0200

    De-indent to 2 spaces
    This is the standard for ruby documents.
    
    Also added an editorconfig, and set it up based on what
    seemed to be the existing rules for the repo.

--- libraries/ruby/lib/standardwebhooks/errors.rb
+++ libraries/ruby/lib/standardwebhooks/errors.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 module StandardWebhooks
-    class StandardWebhooksError < StandardError
-        attr_reader :message
+  class StandardWebhooksError < StandardError
+    attr_reader :message
 
-        def initialize(message = nil)
-            @message = message
-        end
+    def initialize(message = nil)
+        @message = message
     end
+  end
 
-    class WebhookVerificationError < StandardWebhooksError
-    end
+  class WebhookVerificationError < StandardWebhooksError
+  end
 
-    class WebhookSigningError < StandardWebhooksError
-    end
+  class WebhookSigningError < StandardWebhooksError
+  end
 end
