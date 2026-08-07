659f67424f865ba66552a9388485f7273ea54862
Dief Bell <dief@wolvenindustries.co.uk>
2026-08-07 08:42:11 +0100

    Added tests for v1a javascript

--- libraries/javascript/jest.config.js
+++ libraries/javascript/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|js)$": ["ts-jest", { isolatedModules: true }],
+  },
+  transformIgnorePatterns: ["node_modules/(?!@stablelib)"],
 };
